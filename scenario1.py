57f47934fd95b09e013ef6387df9f817450f774e
chenmichael2 <chenmichael2012@gmail.com>
2022-01-14 13:41:58 -0800

    organize

--- scenario1.py
+++ scenario1.py
@@ -1,26 +1,27 @@
-'''
-filter: splitting one chaotic list into smaller, more organized lists
-map: performing a mathematic operation on a list to acquire a new set of values 
-'''
+# '''
+# NEO: filter: splitting one chaotic list into smaller, more organized lists
+# MICHAEL: map: performing a mathematic operation on a list to acquire a new set of values 
+# '''
 
-'''
-Filling out a cafe menu from an array of data and categorizing it by types of food/drink?
-Like map to make the arrays organized for display on a page?
-So like.... Cola, Sprite, Coffee?
-And Filter to display specific data by a type you need so filter(food) and getting back Pancakes, Burgers and not getting drinks bacK?
-'''
+# '''
+# Filling out a cafe menu from an array of data and categorizing it by types of food/drink?
+# NEO : Like map to make the arrays organized for display on a page?
+# So like.... Cola, Sprite, Coffee?
+# MICHAEL : And Filter to display specific data by a type you need so filter(food) and getting back Pancakes, Burgers and not getting drinks bacK?
+# '''
 
-'''
-map - determine the total for items in a store after taxes/discounts
-filter - get a list of items that are listed at a high discount right now
-'''
+# '''
+# NEO : map - determine the total for items in a store after taxes/discounts
+# MICHAEL:filter - get a list of items that are listed at a high discount right now
+# '''
 
-'''
-inflation on products increase to 7% for map, 
-filter is to take out the things i want in a list
-'''
+# '''
+# MICHAEL: inflation on products increase to 7% for map, 
+# NEO: filter is to take out the things i want in a list
+# '''
 
-'''
-use map to get a total value in a cart
-use filter to sort a list by any value
-'''
+# '''
+# MICHAEL: use map to get a total value in a cart
+# NEO: use filter to sort a list by any value
+# '''
+
