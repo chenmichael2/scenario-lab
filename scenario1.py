9886ee6a118cf658cde0eaa539339decbe630a1f
nneofotistos <n_neofotistos@yahoo.com>
2022-01-14 16:52:18 -0600

    new file

--- scenario1.py
+++ scenario1.py
@@ -1,12 +1,5 @@
 # '''
 # NEO: filter: splitting one chaotic list into smaller, more organized lists
-list1 = [1, 22, 45, 2, 36, 81, 79, 12, 8]
-
-result = filter(lambda x: x < 25, list1)
-print(list(result))
-
-result2 = filter(lambda x: x >= 25, list1)
-print(list(result2))
 # MICHAEL: map: performing a mathematic operation on a list to acquire a new set of values 
 # '''
 
@@ -14,10 +7,6 @@ print(list(result2))
 # Filling out a cafe menu from an array of data and categorizing it by types of food/drink?
 # NEO : Like map to make the arrays organized for display on a page?
 # So like.... Cola, Sprite, Coffee?
-cafe_menu = [{'name': 'Pancakes',
-               'type': 'food' }, {'name': 'Burgers'}, 'Steak', 'Cola', 'Sprite', 'Coffee']
-
-food = filter
 # MICHAEL : And Filter to display specific data by a type you need so filter(food) and getting back Pancakes, Burgers and not getting drinks bacK?
 # '''
 
